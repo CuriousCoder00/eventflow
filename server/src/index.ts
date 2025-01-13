672dcf3e123c60b1abfafb8a46430ddca2a74f3d
Kapil Jangid <kapiljangid040@gmail.com>
2025-01-13 23:57:58 +0530

    feat: set up Express server with MongoDB connection and JWT authentication middleware

---
+++ server/src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { PORT } from './lib/config';
+import { db } from './lib/db';
+
+
+// Connect to the database
+db();
+
+// Create an Express app
+const app = express();
+
+// Use the Express JSON parser
+app.use(express.json());
+// Use the Express URL-encoded parser
+app.use(express.urlencoded({ extended: true }));
+// Use the body parser
+app.use(bodyParser.json());
+// Use the CORS middleware
+app.use(cors({ credentials: true, origin: true }));
+// Use the cookie parser
+app.use(cookieParser());
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}).on('error', (error) => {
+    console.error('Error starting server: ', error);
+});
